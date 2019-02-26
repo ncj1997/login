f3e0782f9fcdb85d47aff5ef3f51d56eff7459c5
ncj1997 <45823732+ncj1997@users.noreply.github.com>
2019-02-26 23:54:12 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+var mysql = require('mysql');
+const port =3000;
+const app =express();
+// database connection part**************
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "mydb"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+// ****************************************
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/',function(req,res){
+    res.send('Hello From Server');
+});
+app.post('/eventlog',function(req,res){
+
+    console.log(req.body);
+    res.status(200).send({"message" : "Data received"});
+    con.connect(function() {
+        //if (err) throw err;
+        console.log("Connected!");
+        var name =req.body.event_name;
+        var mod = req.body.moderator;
+        var sql = "INSERT INTO data VALUES ('"+name+"', '"+mod+"')";
+        con.query(sql, function (err, result) {
+          //if (err) throw err;
+          console.log("1 record inserted");
+        });
+      });
+})
+
+
+app.listen(port,function(){
+    console.log("Servce is running on localhost" + port);
+});
